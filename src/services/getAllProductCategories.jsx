f0665891dc8f6fc259f9155ef7c92d677ce1b7db
maulanamb <mmbhaswara@gmail.com>
2024-11-18 23:59:09 +0700

    first commit

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,30 @@
+const getAllProductCategories = ()  => {
+  return [   {
+    id: 'D-001',
+    slug: 'nba-2k25',
+    name: `NBA 2K25`
+  },
+  {
+    id: 'D-002',
+    slug: 'fc-25',
+    name: `EA SPORTS FC 25`
+  },
+  {
+    id: 'ND-003',
+    slug: 'spiderman-remastered',
+    name: `Spiderman Remastered`
+  },
+  {
+    id: 'ND-004',
+    slug: 'hogwarts-legacy',
+    name: `Hogwarts Legacy`
+  },
+  {
+    id: 'D-005',
+    slug: 'naruto-x-boruto-ultimate-ninja-storm-connections',
+    name: `NARUTO X BORUTO Ultimate Ninja STORM CONNECTIONS`
+  },
+  ]
+}
+
+export default getAllProductCategories
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,54 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'D-001',
+      slug: 'nba-2k25',
+      name: "NBA 2K25",
+      category: 'Digital Game',
+      price: 700_000,
+      stock: 100,
+      imageUrl: '/assets/images/nba2k25.png',
+      description: `Command every court with authenticity and realism Powered by ProPLAY™, giving you ultimate control over how you play in NBA 2K25. Define your legacy in MyCAREER, MyTEAM, MyNBA, and The W.`
+    },
+    {
+      id: 'D-002',
+      slug: 'ea-sports-fc-25',
+      name: "EA SPORTS FC 25",
+      category: 'Digital Game',
+      price: 800_000,
+      stock: 5,
+      imageUrl: `/assets/images/fc25.jpeg`,
+      description: `EA SPORTS FC™ 25 gives you more ways to win for the club. Team up with friends in your favourite modes with the new 5v5 Rush, and manage your club to victory as FC IQ delivers more tactical control than ever before.`
+    },
+    {
+      id: 'ND-003',
+      slug: 'spiderman-remastered',
+      name: " Spiderman Remastered",
+      category: 'Non-Digital Game',
+      price: 700_000,
+      stock: 0,
+      imageUrl: `/assets/images/spiderman.jpg`,
+      description: `In Marvel’s Spider-Man Remastered, the worlds of Peter Parker and Spider-Man collide in an original action-packed story. Play as an experienced Peter Parker, fighting big crime and iconic villains in Marvel’s New York. Web-swing through vibrant neighborhoods and defeat villains with epic`
+    },
+    {
+      id: 'ND-004',
+      slug: 'hogwarts-legacy',
+      name: "Hogwarts Legacy",
+      category: 'Non-Digital Game',
+      price: 600_000,
+      stock: 0,
+      imageUrl: `/assets/images/hogwarts.jpg`,
+      description: `Hogwarts Legacy is an immersive, open-world action RPG. Now you can take control of the action and be at the center of your own adventure in the wizarding world.`
+    },
+    {
+      id: 'D-005',
+      slug: 'naruto-x-boruto-ultimate-ninja-storm-connections',
+      name: "NARUTO X BORUTO Ultimate Ninja STORM CONNECTIONS",
+      category: 'Digital Game',
+      price: 650_000,
+      stock: 50,
+      imageUrl: `/assets/images/naruto.png`,
+      description: `Legendary ninjas reunite in the next exciting entry of the STORM series! NARUTO X BORUTO Ultimate Ninja STORM CONNECTIONS is an action/fighting game with fast-paced ninja battles and two different story modes.`
+    },
+  ]
+}
